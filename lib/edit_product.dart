aeeaa052a72355356ee9baa1d59d7f3db1e78ac4
gech11 <getachewfikadu77@gmail.com>
2021-12-04 23:59:55 +0300

    initial commit

--- lib/edit_product.dart
+++ lib/edit_product.dart
@@ -10,7 +10,41 @@ class _EditProductState extends State<EditProduct> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('phone number',style:TextStyle(fontSize:25,fontWeight:FontWeight.bold)),
+          SizedBox(height:20),
+          TextField(
+            decoration:textInputDecoration.copyWith(labelText:'phone',hintText:'phone number'),
+            controller:_productNameController,
+          ),
+          Text('city',style:TextStyle(fontSize:25,fontWeight:FontWeight.bold)),
+          SizedBox(height:20),
+          TextField(
+            decoration:textInputDecoration.copyWith(labelText:'city',hintText:'city'),
+            controller:_productNameController,
+          ),
+          Text('street',style:TextStyle(fontSize:25,fontWeight:FontWeight.bold)),
+          SizedBox(height:20),
+          TextField(
+            decoration:textInputDecoration.copyWith(labelText:'street',hintText:'street'),
+            controller:_productNameController,
+          ),
+          TextButton(
+            onPressed: (){
+
+            },
+            child: Text(
+              'edit profile',
+              style:TextStyle(
+                backgroundColor: Colors.green,
+                color:Colors.white
+              )
+              )
+              )
+        ],
+      ),
     );
   }
 }
