e1c57514f6e92d622f3f8ea8205950ca2e84f3ee
gech11 <getachewfikadu77@gmail.com>
2021-12-04 23:54:29 +0300

    initial commit

--- lib/comment_card.dart
+++ lib/comment_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ecommerce/models/comment.dart';
+
 class CommentCard extends StatefulWidget {
-  const CommentCard({ Key? key }) : super(key: key);
+  Comment comment=Comment();
+  const CommentCard({ Key? key,this.comment}) : super(key: key);
 
   @override
   _CommentCardState createState() => _CommentCardState();
@@ -10,7 +13,31 @@ class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding:EdgeInsets.all(2),
+      margin:EdgeInsets.all(2),
+      child:ListView(
+        leading:CircleAvatar(
+          backgroundImage:NetworkImage('http://127.0.0.1:8000/user/avatar'),
+          radius:25
+        ),
+        title:Text(comment.body,style:TextStyle(fontSize:20)),
+        subtitle:Row(
+          IconButton(
+            onpressed:(){
+
+            },
+            icon:Icon(Icons.thumbup),
+            size:25
+          ),
+          IconButton(
+            onpressed:(){
+
+            },
+            icon:Icon(Icons.thumbup),
+            size:25
+          )
+        )
+      )
     );
   }
 }
--- lib/create_product.dart
+++ lib/create_product.dart
@@ -11,7 +11,32 @@ class _CreateProductState extends State<CreateProduct> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child:Column(
+        childeren:[
+          Text('sell your products here',style:TextStyle(fontSize:20,fontWeight:FontWeight.bold)),
+          SizedBox(height:20),
+          Text('product name',style:TextStyle(fontSize:20,fontWeight:FontWeight.bold)),
+          SizedBox(height:20),
+          TextFormField(
+           decoration:textInputDecoration.copyWith(labelText:'name',hintText:'product name'),
+           validator:(val)=>val.length>1 null : 'product name should be minimum of 2 chars',
+           controller:_productNameController,
+          ),
+          SizedBox(height:20),
+          TextFormField(
+           decoration:textInputDecoration.copyWith(labelText:'description',hintText:'product description'),
+           controller:_productNameController,
+          ),
+          SizedBox(height:20),
+          TextButton(
+            onpressed:(){
+
+            },
+            color:Colors.crimson,
+            child:Text('sell',style:TextStyle(color:Colors.white))
+          )
+        ]
+      )
     );
   }
 }
